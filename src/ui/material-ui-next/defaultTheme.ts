4936ad72cd4436c603a5a9f950058e9f86a26573
Mathias <mathias@kandelborg.dk>
2018-03-28 17:00:03 +0200

    refactor: Only use the email input in App and stories & renaming things
    Also make defaultTheme an instance of Theme

--- src/ui/material-ui-next/defaultTheme.ts
+++ src/ui/material-ui-next/defaultTheme.ts
@@ -1,11 +1,11 @@
-import { createMuiTheme } from 'material-ui/styles';
+import { createMuiTheme, Theme } from 'material-ui/styles';
 import orange from 'material-ui/colors/orange';
 import pink from 'material-ui/colors/pink';
 import red from 'material-ui/colors/red';
 
 // All the following keys are optional.
 // We try our best to provide a great default value.
-export const defaultTheme = createMuiTheme({
+export const defaultTheme: Theme = createMuiTheme({
   typography: {
     // Tell Material-UI what's the font-size on the html element is.
     htmlFontSize: 16,
